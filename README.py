b7bf6848b2cad3d218964e7354dff359c8303898
K4 <47014074+iK4oS@users.noreply.github.com>
2021-10-30 22:00:36 +0000

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,16 @@
+from GitHub import ReadMe
+
+class iK4oS(ReadMe):
+
+    def __init__(self):
+        self.username = "iK4oS"
+        self.contacts = {
+            "Discord": "iK4oS"
+        }
+        self.alises = {
+            "K4oS"
+        }
+        self.occupation = "IT Student"
+        self.projects = {
+            "backdoor.exe": "https://github.com/iK4oS/backdoor.exe"
+        }
